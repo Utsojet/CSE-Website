aec9b0db1a0bc021fbfefb8e5287b61251de38d8
Utsojet <125271400+Utsojet@users.noreply.github.com>
2025-08-20 12:11:44 +0600

    Add files via upload
    submit all file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Alert on contact form submission
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting us! We will get back to you soon.");
+});
